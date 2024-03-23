2b229bcb928a673121fd1b455b977b945c5a3c88
suyatinglaura <e0492464@u.nus.edu>
2024-03-23 21:26:25 +0800

    update PCToken

--- PCToken.sol
+++ PCToken.sol
@@ -13,19 +13,15 @@ contract PCToken {
     }
     /**
     * @dev Function to give PCT to the recipient for a given wei amount
-    * @param recipient address of the recipient that wants to buy the PCT
-    * @param weiAmt uint256 amount indicating the amount of wei that was passed
-    * @return A uint256 representing the amount of DT bought by the msg.sender.
+    * @return A uint256 representing the amount of DT bought by the msg.sender
     */
     // Currently, 1 PCT = 0.01Eth
-    function getCredit(address recipient, uint256 weiAmt)
-        public
-        returns (uint256)
-    {
-        uint256 amt = weiAmt / (1000000000000000000/100); 
-        erc20Contract.mint(recipient, amt);
+    function getCredit() public payable returns (uint256) {
+        uint256 amt = msg.value / (1000000000000000000/100); 
+        erc20Contract.mint(msg.sender, amt);
         return amt; 
     }
+
     /**
     * @dev Function to check the amount of PCT the msg.sender has
     * @param ad address of the recipient that wants to check their PCT
@@ -35,6 +31,7 @@ contract PCToken {
         uint256 credit = erc20Contract.balanceOf(ad);
         return credit; 
     }
+    
     /**
     * @dev Function to transfer the credit from the owner to the recipient
     * @param recipient address of the recipient that will gain in PCT
@@ -44,6 +41,4 @@ contract PCToken {
         // Transfers from tx.origin to receipient
         erc20Contract.transfer(recipient, amt);
     }
-
-
 }
